aabd1a8b5faf52ffe3e496d4059423ba780e43ae
Seth Steenken <sethsteenken@gmail.com>
2018-10-09 08:39:59 -0400

    Initial service collection extensions. WIP

--- src/Bunder/Configuration/BunderServiceCollectionExtensions.cs
+++ src/Bunder/Configuration/BunderServiceCollectionExtensions.cs
@@ -1,10 +1,78 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Bunder
 {
     public static class BunderServiceCollectionExtensions
     {
+        internal static readonly IDictionary<string, string> _defaultOutputDirectoryLookup = new Dictionary<string, string>()
+        {
+            { "js", "/content/js" },
+            { "css", "/content/css" }
+        };
+
+        public static IServiceCollection AddBunder(this IServiceCollection services)
+        {
+            return null;
+
+        }
+
+
+        public static IServiceCollection AddBunder(this IServiceCollection services, BunderSettings settings)
+        {
+            return null;
+        }
+
+        public static IServiceCollection AddBunder(this IServiceCollection services, IBundlingConfiguration bundlingConfiguration)
+        {
+            return null;
+        }
+
+
+        public static IServiceCollection AddBunder(this IServiceCollection services, BunderSettings settings, IBundlingConfiguration bundlingConfiguration)
+        {
+            Guard.IsNotNull(services, nameof(services));
+            Guard.IsNotNull(settings, nameof(settings));
+            Guard.IsNotNull(bundlingConfiguration, nameof(bundlingConfiguration));
+
+            services.TryAddSingleton<JsonSerializer>();
+
+            services.TryAddSingleton<BunderSettings>(settings);
+            services.TryAddSingleton<IBundlingConfiguration>((serviceProvider) =>
+            {
+                string configPath = Path.Combine(serviceProvider.GetRequiredService<IHostingEnvironment>().ContentRootPath,
+                                                serviceProvider.GetRequiredService<BunderSettings>().BundlesConfigFilePath);
+
+                return new BundlingJsonConfiguration(_defaultOutputDirectoryLookup, 
+                                serviceProvider.GetRequiredService<JsonSerializer>(),
+                                configPath);
+            });
+
+            services.TryAddSingleton<IEnumerable<Bundle>>((serviceProvider) => serviceProvider.GetRequiredService<IBundlingConfiguration>().Build());
+            services.TryAddSingleton<IBundleLookup, BundleLookup>();
+            services.TryAddSingleton<IVersioningFormatter>((serviceProvider) =>
+            {
+                return new FileVersioningFormatter(
+                    serviceProvider.GetRequiredService<IHostingEnvironment>().WebRootFileProvider,
+                    serviceProvider.GetRequiredService<IMemoryCache>()
+                );
+            });
+
+            services.TryAddScoped<IPathFormatter, UrlPathFormatter>();
+            services.TryAddScoped<IAssetResolver, AssetResolver>();
+
+
+            //serviceProvider.GetRequiredService<IHttpContextAccessor>().HttpContext.Request.PathBase
+
+            return services;
+        }
     }
 }
---
+++ src/Bunder/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace Bunder.Extensions
+{
+    internal static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Register a custom class and interface that represents a given configuration section on the <see cref="IConfiguration"/> instance.
+        /// </summary>
+        /// <typeparam name="TSettingsInterface">Interface that represents the configuration section.</typeparam>
+        /// <typeparam name="TSettingsType">Class that represents the configuration section.</typeparam>
+        /// <param name="services">Existing service collection.</param>
+        /// <param name="sectionName">Name of the section found in <see cref="IConfiguration"/> that will be read into <typeparamref name="TSettingsInterface"/> and <typeparamref name="TSettingsType"/>.</param>
+        /// <param name="singleton">Whether or not <typeparamref name="TSettingsInterface"/> and <typeparamref name="TSettingsType"/> is registered as a singleton. If not, configuration settings can be altered without an application restart at the cost of performance.</param>
+        /// <returns></returns>
+        public static IServiceCollection AddCustomConfigurationSettings<TSettingsInterface, TSettingsType>(this IServiceCollection services, string sectionName, bool singleton)
+            where TSettingsInterface : class
+            where TSettingsType : class, TSettingsInterface, new()
+        {
+            Guard.IsNotNull(services, nameof(services));
+
+            var section = GetConfigurationSection(services, sectionName);
+            services.Configure<TSettingsType>(section);
+
+            // Register as singleton or scoped. 
+            // Singleton is one instance for the app. 
+            // Scoped settings will be read on every request (no app restart on setting changes at the drawback of performance)
+            // IOptionsSnapshot is also registered under Scoped to allow this always-updated feature.
+            if (singleton)
+            {
+                services.AddSingleton<TSettingsType>(sp => sp.GetRequiredService<IOptions<TSettingsType>>().Value);
+                services.AddSingleton<TSettingsInterface>(sp => sp.GetRequiredService<TSettingsType>());
+            }
+            else
+            {
+                services.AddScoped<TSettingsType>(sp => sp.GetRequiredService<IOptionsSnapshot<TSettingsType>>().Value);
+                services.AddScoped<TSettingsInterface>(sp => sp.GetRequiredService<TSettingsType>());
+            }
+
+            return services;
+        }
+
+        /// <summary>
+        /// Get a specific section by name from the registered <see cref="IConfiguration"/>.
+        /// </summary>
+        /// <param name="services">Existing service collection.</param>
+        /// <param name="sectionName">Name of the section in the configuration instance.</param>
+        /// <returns></returns>
+        public static IConfigurationSection GetConfigurationSection(this IServiceCollection services, string sectionName)
+        {
+            Guard.IsNotNull(services, nameof(services));
+            Guard.IsNotNull(sectionName, nameof(sectionName));
+
+            var section = services.BuildServiceProvider().GetRequiredService<IConfiguration>().GetSection(sectionName);
+            if (section == null || !section.Exists())
+                throw new ApplicationException($"Configuration section '{sectionName}' was not found.");
+            return section;
+        }
+    }
+}
